3fcbce6e348f4b85122b649beb22df7ba727e317
Anderson-Wes <anderson.wes420@gmail.com>
2025-09-21 21:22:12 +0400

    ALLOWED_HOSTS

--- wedding_site/settings.py
+++ wedding_site/settings.py
@@ -39,6 +39,7 @@ CSRF_TRUSTED_ORIGINS = [
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
